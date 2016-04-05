efe37d0a25d5f7c96b429650e159a142ecc7da99
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2016-04-06 08:33:25 +0900

    sample okhttp and retrofit

--- app/src/main/java/com/os/operando/myandroidtemplate/MainActivity.java
+++ app/src/main/java/com/os/operando/myandroidtemplate/MainActivity.java
@@ -6,10 +6,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+import com.google.gson.annotations.SerializedName;
 import com.jakewharton.rxbinding.widget.RxTextView;
 import com.os.operando.myandroidtemplate.databinding.ActivityMainBinding;
 
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 import rx.Observable;
+import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
@@ -50,5 +67,119 @@ public class MainActivity extends AppCompatActivity {
         Observable.combineLatest(nameWritten, passWritten, (aBoolean, aBoolean2) -> aBoolean && aBoolean2)
                 .subscribe(isValid -> binding.button.setEnabled(isValid));
 
+        String id = "2172797";
+        String appId = "464b981be2248f383abxxxxxxxxxxx";
+        String path = "data/2.5/weather";
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpclient = new OkHttpClient.Builder()
+                .addInterceptor(new RequestHeaderInterceptor())
+                .addInterceptor(loggingInterceptor)
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .client(okHttpclient)
+                .baseUrl("http://api.openweathermap.org")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+
+        retrofit.create(IWhetherApi.class).getWhether(path, id, appId)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<WetherResponse>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(WetherResponse wetherResponse) {
+                        Log.d("next", String.valueOf(wetherResponse.main.tempMin));
+                    }
+                });
+    }
+
+    class RequestHeaderInterceptor implements Interceptor {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            final Request.Builder builder = chain.request().newBuilder();
+            builder.addHeader("headerKey", "value");
+            return chain.proceed(builder.build());
+        }
+    }
+
+    interface IWhetherApi {
+
+        @GET("/{fileName}")
+        Observable<WetherResponse> getWhether(@Path(value = "fileName", encoded = false) String fileName,
+                                              @Query("id") String id,
+                                              @Query("APPID") String appId);
+    }
+
+    class WetherResponse {
+        public Coord coord;
+        @SerializedName("weather")
+        public List<Weather> weathers;
+        public String base;
+        public Main main;
+        public int visibility;
+        public Wind wind;
+        @SerializedName("clouds")
+        public Cloud cloud;
+        public int dt;
+        public Sys sys;
+        public int id;
+        public String name;
+        public int cod;
+
+        public class Coord {
+            public double lon;
+            public double lat;
+        }
+
+        public class Weather {
+            public int id;
+            public String main;
+            public String description;
+            public String icon;
+        }
+
+        public class Main {
+            public double temp;
+            public int pressure;
+            public int humidity;
+            @SerializedName("temp_min")
+            public double tempMin;
+            @SerializedName("temp_max")
+            public double tempMax;
+        }
+
+        public class Wind {
+            public double speed;
+            public double deg;
+        }
+
+        public class Cloud {
+            public int all;
+        }
+
+        public class Sys {
+            public int type;
+            public int id;
+            public double message;
+            public String country;
+            public int sunrise;
+            public int sunset;
+        }
     }
 }
