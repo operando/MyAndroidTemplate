548596df8b20c3eedf307b650276d5fdf61b10c9
operando <okanoshinobu8@gmail.com>
2016-05-14 21:42:48 +0900

    Add test module

---
+++ appAndroidTest/src/main/java/com/os/operando/myandroidtemplate/test/SampleTest.java
@@ -0,0 +1,17 @@
+package com.os.operando.myandroidtemplate.test;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertFalse;
+
+@RunWith(AndroidJUnit4.class)
+public class SampleTest {
+
+    @Test
+    public void test() {
+        assertFalse(false);
+    }
+}
